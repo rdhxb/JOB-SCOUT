bcb1a3f35826fe04df457589528e0becc26393d0
rdhxb <andrjespl@wp.pl>
2025-08-13 23:39:49 +0200

    Refactor job search URL construction and add input variables for job search parameters

--- src/main.py
+++ src/main.py
@@ -6,10 +6,23 @@ TARGET_URL = 'https://www.pracuj.pl/'
 POSITION = str(input('Podaj stanowisko jakiego szukasz > ')).lower()
 CITY = str(input('Podaj miasto  > ')).lower()
 DISTANCE = int(input(f'podaj maksymalna odleglosc od {CITY} > '))
+JOB_LEVEL = 'bedzie wybieral z listy bo bedzie dict np junior == 17 , mid == 4 itp. bo na pracuj pl tak jest i chuj '
+TYPE_OF_CONTRACT = 'tak samo'
+SHOW_SALLARY = 'input 0 - 1 wystaryczy i wtedy do url dodamy'
+WORKLOAD = 'SAME'
+REMOTE_TYPE = 'SAME'
+PUBLISH_TIME = 'to kaloes dziwne sie ogarnie jak sie uda CLI zrobic wybierane to bedzie git'
 
-print(CITY)
 
-TARGET_URL = f'{TARGET_URL}praca/{POSITION};kw/{CITY};wp?rd={DISTANCE}'
+
+if ' ' in POSITION:
+    first, sec = POSITION.split(' ', 1)
+
+    print(f'first = {first} sec = {sec}')
+    TARGET_URL = f'{TARGET_URL}praca/{first}%20{sec};kw/{CITY};wp?rd={DISTANCE}&ao=false' #ao false to zeby bylo bezxposrednio od pracodawcy
+else:
+    TARGET_URL = f'{TARGET_URL}praca/{POSITION};kw/{CITY};wp?rd={DISTANCE}&ao=false'
+
 print(TARGET_URL)
 try:
     response = requests.get(TARGET_URL)
@@ -25,6 +38,6 @@ except:
 
 # https://it.pracuj.pl/praca/it;kw/warszawa;wp?rd=20
 # https://it.pracuj.pl/praca/sales%20engineer;kw/lublin;wp?rd=30
-
+# https://it.pracuj.pl/praca/administrator%20it;kw
 # URL looks like 
 # https://it.pracuj.pl/praca/ Stanowisko, slowo_klucz itp. ;kw/ miasto z malej ;wp/?rd = ilosc  kilometrow od 
